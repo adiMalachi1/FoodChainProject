8d001c204364d590b603f84739d89e196fce64eb
Adi Malachi <adida901@gmail.com>
2021-12-22 20:36:43 +0200

    Update HomeScreen.js
    Added navigating between screens -  here move to sign in screen tamplate

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,52 +1,77 @@
-import React, { useContext } from 'react';
-import { View, Text,Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet,
+    Image,
+} from 'react-native';
 
+const HomeScreen = ({navigation}) => {
+    return (
+      
+        <View style = { Styles.container}>
+            <Image
+                source={require('../assets/logoFoodChain.png')}
+                style={Styles.logo}
+            />
+            <View>
+                <Text style={Styles.textSecondHeader}>Food chain app</Text>
+            </View>
+            <View> 
+            <Text style ={Styles.textheader}>Welcome!</Text>
+            </View>   
 
-const HomeScreen = () => {
-  return (   
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/logoFoodChain.png')}
-        style={styles.logo}
-      />
-      <View>
-      <Text style={[styles.textColor, {
-            fontSize: 30,
-            paddingBottom:20,
-            paddingTop:20,
-          }]}>Food chain app</Text>
-      </View>
-      <View> 
-        <Text style ={styles.textheader}>Welcome!</Text>
-      </View> 
-    </View>
-    
-  );
-}
+            <TouchableOpacity onPress={()=> navigation.navigate('SignInScreen')}
+                style={Styles.conTouch} 
+            ><Text style = {[Styles.textColor,{fontSize: 20,}]}>המשך</Text></TouchableOpacity>
+
+        </View> 
+    );
+};
+
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#009387',
-    alignItems: 'center',
-    paddingTop: 100, 
-  },
-  textColor:{
-    fontWeight: 'bold',
-    color: 'white'
-  },
-  textheader:{
-    fontWeight: 'bold',
-    color: 'white',
-    fontSize: 40
-  },
-  logo: {
-    height: 100,
-    width: 100,
-    borderRadius: 10,
-    paddingBottom: 100,
-    
-  },
-
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#009387',
+        alignItems: 'center',
+        paddingTop: 100, 
+      },
+      textColor:{
+        fontWeight: 'bold',
+        color: 'white',
+      },
+      textheader:{
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 40,
+        paddingBottom:200,
+      },
+      textSecondHeader:{
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 30,
+        paddingBottom:20,
+        paddingTop:20,
+      },
+      logo: {
+        height: 100,
+        width: 100,
+        borderRadius: 10,
+        paddingBottom: 100,
+        paddingVertical: 10,
+        resizeMode: 'cover',
+      },
+      conTouch :{
+        borderWidth:2,
+        borderColor:'#fff',
+        alignItems:'center',
+        justifyContent:'center',
+        width:150,
+        height:50,
+        backgroundColor:'#009387',
+        borderRadius:10,
+        },
+   
 });
