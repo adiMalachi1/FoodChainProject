74456e801d94d3a75122d7d567433cda1659eac2
Adi Malachi <adida901@gmail.com>
2022-01-03 18:31:04 +0200

    Update RootStackScreen.js

--- Screens/RootStackScreen.js
+++ Screens/RootStackScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
+import LoginScreen from './LoginScreen';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignupScreen';
 
@@ -11,6 +12,7 @@ const RootStackScreen = ({navigation}) => (
         headerShown: false
       }}>
         <RootStack.Screen name="HomeScreen" component={HomeScreen}/>
+        <RootStack.Screen name="LoginScreen" component={LoginScreen}/>
         <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
         <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
     </RootStack.Navigator>
