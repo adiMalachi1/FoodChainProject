4187f8f7ca5b6d31e40a953fcc7e320bf3e95391
Adi Malachi <adida901@gmail.com>
2022-02-14 13:22:10 +0200

    Update FirebaseConfig.js

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,25 +1,24 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import database from 'firebase/compat/database'
 
+// import database from '@react-native-firebase/database'
 const firebaseConfig = {
   apiKey: "AIzaSyAF0FeQYSfcVAqQHnRWjMJhwiCwu0mzsl0",
   authDomain: "foodchainproject-ec30e.firebaseapp.com",
   projectId: "foodchainproject-ec30e",
   storageBucket: "foodchainproject-ec30e.appspot.com",
   messagingSenderId: "53743036595",
-  appId: "1:53743036595:web:834e0ca4ccdaddbb0ce8c6"
+  appId: "1:53743036595:web:834e0ca4ccdaddbb0ce8c6",
+  databaseURL: "https://foodchainproject-ec30e-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
-let app;
 
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig)
-} else {
-  app = firebase.app();
-}
+if (firebase.apps.length === 0) 
+ firebase.initializeApp(firebaseConfig)
 
-const db = app.firestore();
+const db = firebase.database();
 const auth = firebase.auth();
 
 export { db, auth };
