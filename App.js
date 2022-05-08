07903bdfb0ecd37dd72be5464727895f9bb65835
Adi Malachi <adida901@gmail.com>
2022-05-08 17:58:32 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from "react";
-import RootStackScreen from './Screens/RootStackScreen';
+import RootStackScreen from "./navigation/RootStackScreen";
 import { 
   NavigationContainer, 
   DefaultTheme as NavigationDefaultTheme,
   DarkTheme as NavigationDarkTheme
 } from '@react-navigation/native';
-import Tabs from './navigation/tabs'
+
+
+import { LogBox} from "react-native";
+
 const App = () => {
+  LogBox.ignoreLogs(['Setting a timer for a long period of time'])
+  LogBox.ignoreLogs(['Each child in a list should have a unique "key" prop'])
+
   return(
     <NavigationContainer>
+       
       <RootStackScreen>
-        {/* <Tabs/> */}
+
       </RootStackScreen>
+
     </NavigationContainer>
   )
-}
+  }
 export default App;
+
