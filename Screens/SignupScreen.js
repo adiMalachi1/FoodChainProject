04f7cda6bab09748bf46ee60add12d9d15828c6c
Adi Malachi <adida901@gmail.com>
2022-05-08 18:15:26 +0300

    Update SignupScreen.js

--- Screens/SignupScreen.js
+++ Screens/SignupScreen.js
@@ -12,6 +12,7 @@ import {
     Alert
 } from 'react-native';
 import Recaptcha from 'react-native-recaptcha-that-works';
+import { color } from '../utils';
 
 const SignUpScreen = ({navigation}) => {
     const [checked, setChecked] = React.useState('giver');
@@ -26,17 +27,13 @@ const SignUpScreen = ({navigation}) => {
      
     // }
     function ValidatePhoneNumber(phoneNumber) { // check if the cell phone number is valid for israel
-        // var regex = /^0(5[^7]|[2-4]|[8-9]|7[0-9])[0-9]{7}$/;
         var regex = /^05\d([-]{0,1})\d{7}$/;
         var phone = phoneNumber.match(regex);
         if (phone) {
-        //   alert('yes');
           return true;
         }
-        // alert('no');
         return false;
       }
-    // ValidatePhoneNumber("03997252")
     const onVerify = token => {
         console.log('success!', token);
         if(checked === 'giver'){
@@ -47,7 +44,6 @@ const SignUpScreen = ({navigation}) => {
         else{
             navigation.navigate('FormGetter')
         }
-            // setKey(token);
     
     }
     
@@ -58,119 +54,98 @@ const SignUpScreen = ({navigation}) => {
 
         if(user == "" ||  email== "" || phone == "" || password == "" || confirmPassword == "" ){
             if(user == ""){
-                 Alert.alert('', "הודעת שגיאה: שם ארגון הינו שדה חובה, אנא הזן אותו",[{},{},{text:"אישור"}])
+                 Alert.alert('', "הודעת שגיאה: שם ארגון הינו שדה חובה, אנא הזן אותו",[,,{text:"אישור"}])
                  return;
             }
             if(email == ""){
-                Alert.alert('', "הודעת שגיאה: אמייל הינו שדה חובה, אנא הזן אותו",[{},{},{text:"אישור"}])
+                Alert.alert('', "הודעת שגיאה: אמייל הינו שדה חובה, אנא הזן אותו",[,,{text:"אישור"}])
                 return;
            }
            if(phone == "" || ValidatePhoneNumber(phone)=== false){
             if(phone == ""){
-                Alert.alert('', "הודעת שגיאה: מס' פלאפון הינו שדה חובה, אנא הזן אותו",[{},{},{text:"אישור"}])
+                Alert.alert('', "הודעת שגיאה: מס' פלאפון הינו שדה חובה, אנא הזן אותו",[,,{text:"אישור"}])
                 return;
             }
             else{
-                Alert.alert('', "הודעת שגיאה: מס' הפלאפון אינו תקין, אנא הזן את המספר הסלולרי שלך",[{},{},{text:"אישור"}])
+                Alert.alert('', "הודעת שגיאה: מס' הפלאפון אינו תקין, אנא הזן את המספר הסלולרי שלך",[,,{text:"אישור"}])
                 return;
             }
            }
            if(password == "" || password.length < 6  ){
                if(password == ""){
-                Alert.alert('', "הודעת שגיאה: סיסמא הינה שדה חובה, אנא הזן אותה",[{},{},{text:"אישור"}])
+                Alert.alert('', "הודעת שגיאה: סיסמא הינה שדה חובה, אנא הזן אותה",[,,{text:"אישור"}])
                 return;
                }
                 else{
-                    Alert.alert('', "הודעת שגיאה: הסיסמא צריכה להיות בעלת 6 תווים לפחות",[{},{},{text:"אישור"}])
+                    Alert.alert('', "הודעת שגיאה: הסיסמא צריכה להיות בעלת 6 תווים לפחות",[,,{text:"אישור"}])
                     return;
                 }
             }
             if(confirmPassword == "" ){  
-              Alert.alert('', "הודעת שגיאה: אימות סיסמא הינה שדה חובה, אנא הזן אותה",[{},{},{text:"אישור"}])
+              Alert.alert('', "הודעת שגיאה: אימות סיסמא הינה שדה חובה, אנא הזן אותה",[,,{text:"אישור"}])
               return;
             }
-            // if(confirmPassword != password){
-            // //   if(password !== confirmPassword){
-            //     alert("kvgi")
-            //     Alert.alert('', "הודעת שגיאה: הסיסמאות אינן תואמות, אנא נסה שנית",[{},{},{text:"אישור"}])
-            //     return;
-            // //   }
-            // }
-            
-                
+           
         }
-        // else if(ValidatePhoneNumber(phone)=== false){
-        //     Alert.alert('', "הודעת שגיאה: מס' הפלאפון אינו תקין, אנא הזן את המספר הסלולרי שלך",[{},{},{text:"אישור"}])
-        //     return;
-        // }
-        // else if( password.length < 6 ){
-        //     Alert.alert('', "הודעת שגיאה: הסיסמא צריכה להיות בעלת 6 תווים לפחות",[{},{},{text:"אישור"}])
-        //     return;
-        // }
-
+       
         else if(confirmPassword !== password){
-            Alert.alert('', "הודעת שגיאה: הסיסמאות אינן תואמות, אנא נסה שנית",[{},{},{text:"אישור"}])
+            Alert.alert('', "הודעת שגיאה: הסיסמאות אינן תואמות, אנא נסה שנית",[,,{text:"אישור"}])
             return;
         }
         else if(ValidatePhoneNumber(phone)=== false){
-            Alert.alert('', "הודעת שגיאה: מס' הפלאפון אינו תקין, אנא הזן את המספר הסלולרי שלך",[{},{},{text:"אישור"}])
+            Alert.alert('', "הודעת שגיאה: מס' הפלאפון אינו תקין, אנא הזן את המספר הסלולרי שלך",[,,{text:"אישור"}])
             return;
         }
         else{
+            
+            auth.createUserWithEmailAndPassword(email,password)
+                .then((res) => {
+                    if (auth.currentUser) {
+                        const userid = auth.currentUser.uid;
+                        if (userid) {
+                            // alert("y")
+                            db.ref('users/' + res.user.uid ).set({
+                                userName: user,
+                                email: email,
+                                phone: phone, 
+                                password: password,
+                                type:checked,
+                                
+                            })
+                            // alert("njk")
+                $recaptcha.current.open();
 
-        auth.createUserWithEmailAndPassword(email,password)
-        // .then(userCredentials => {
-        //     const user = userCredentials.user;
-        //     console.log('Register with:',user.email)
-        // })
-        // .catch(error=>alert(error.message))
-        .then((res) => {
-             if (auth.currentUser) {
-                 const userid = auth.currentUser.uid;
-                if (userid) {
-                    db.ref('users/' + res.user.uid ).set({
-                         userName: user,
-                         email: email,
-                         phone: phone, 
-                         password: password,
-                         type:checked,
-                         
-                    })
-                     $recaptcha.current.open();
+                            } 
 
-                }
-
-                
-                
-
-            }
-        })
-        .catch((error) => {
-            // console.log(error.message);
-            switch(error.message) {
-                case 'Firebase: The email address is badly formatted. (auth/invalid-email).':
-                Alert.alert('', "הודעת שגיאה: כתובת אמייל לא תקינה",[{},{},{text:"אישור"}])
-                break;
-        }})
-        // $recaptcha.current.open();
-
-        // if(checked === 'giver'){
-        //     $recaptcha.current.open();
-
-        //     // navigation.navigate('FormGiver')           
-
-        // }
-        // else{
-        //     navigation.navigate('FormGetter')
-        // }
-    }
+                    }})
+                .catch((error) => {
+                    console.log(error.message);
+                    switch(error.message) {
+                        case 'Firebase: The email address is badly formatted. (auth/invalid-email).':
+                        Alert.alert('', "הודעת שגיאה: כתובת אמייל לא תקינה",[,,{text:"אישור"}])
+                        break;
+                        case 'The email address is badly formatted.':
+                        Alert.alert('', "הודעת שגיאה: כתובת אמייל לא תקינה",[,,{text:"אישור"}])
+                        break;
+                        case 'Firebase: The email address is already in use by another account. (auth/email-already-in-use).':
+                        Alert.alert('', "הודעת שגיאה: כתובת אמייל זו כבר קיימת במערכת",[,,{text:"אישור"}])
+                        break;
+                        case 'The email address is already in use by another account.':
+                        Alert.alert('', "הודעת שגיאה: כתובת אמייל זו כבר קיימת במערכת",[,,{text:"אישור"}])
+                        break;
+                        case 'Firebase: The email address is already in use by another account.':
+                        Alert.alert('', "הודעת שגיאה: כתובת אמייל זו כבר קיימת במערכת",[,,{text:"אישור"}])
+                        break;
+                }})
+   
         }
+    }
     return (
-        
+    
     <View style ={Styles.container}>
     
         <Text style = {Styles.textDetail}>נא הזן את הפרטים שלך</Text>
-        <Text  style={{marginVertical:10}}>שם הארגון</Text>
+        <Text  style={{marginVertical:10, writingDirection:'rtl'}}>שם הארגון</Text>
 
         <TextInput
         value={user}
@@ -182,7 +157,7 @@ const SignUpScreen = ({navigation}) => {
         style = {Styles.textInput}
         autoFocus = {true}
       />
-       <Text  style={{marginVertical:10}}>אמייל</Text>
+       <Text  style={{marginVertical:10,writingDirection:'rtl'}} >אמייל</Text>
 
        <TextInput
         value={email}
@@ -193,7 +168,7 @@ const SignUpScreen = ({navigation}) => {
         autoCorrect={false}
         style = {Styles.textInput}
       />
-       <Text  style={{marginVertical:10}}>פלאפון סלולרי</Text>
+       <Text  style={{marginVertical:10,writingDirection:'rtl'}}>פלאפון סלולרי</Text>
 
        <TextInput
         value={phone}
@@ -204,7 +179,7 @@ const SignUpScreen = ({navigation}) => {
         autoCorrect={false}
         style = {Styles.textInput}
       />
-       <Text  style={{marginVertical:10}}>סיסמא</Text>
+       <Text  style={{marginVertical:10,writingDirection:'rtl'}}>סיסמא</Text>
 
        <TextInput
         labelValue={password}
@@ -216,7 +191,7 @@ const SignUpScreen = ({navigation}) => {
         autoCorrect={false}
         style = {Styles.textInput}
       />
-       <Text  style={{marginVertical:10}}>אימות סיסמא</Text>
+       <Text  style={{marginVertical:10,writingDirection:'rtl'}}>אימות סיסמא</Text>
 
        <TextInput
         value={confirmPassword}
@@ -228,6 +203,7 @@ const SignUpScreen = ({navigation}) => {
         autoCorrect={false}
         style = {Styles.textInput}
       />
+      
       
     <View style={{ flexDirection: 'row', justifyContent: 'center',margin:10,}}>
         <View>
@@ -256,7 +232,6 @@ const SignUpScreen = ({navigation}) => {
         <Recaptcha
           ref={$recaptcha}
           lang="iw"//for hebrew
-
           siteKey="6Lful3YeAAAAAO13Y30Zsnx33PuTG0ms3xJ6-VXN"
           baseUrl="http://127.0.0.1"
           onError={(err) => {
@@ -269,10 +244,9 @@ const SignUpScreen = ({navigation}) => {
          />
         <View style = { Styles.center}>
         <TouchableOpacity
-            onPress={()=>{handleSignUp();}}
-            // onPress = {()=> alert(checked)}
-                style={Styles.conTouch} 
-            ><Text style = {[Styles.textColor,{fontSize: 20,}]}>המשך</Text></TouchableOpacity>
+                onPress={()=>{handleSignUp();}}
+                style={Styles.conTouch}  >
+            <Text style = {[Styles.textColor,{fontSize: 20,}]}>המשך</Text></TouchableOpacity>
         </View>
 
     </View>
@@ -283,10 +257,9 @@ const SignUpScreen = ({navigation}) => {
 export default SignUpScreen;
 const Styles = StyleSheet.create({
     container: {
-        backgroundColor: '#009387',
+        backgroundColor: color.WHITE,
         flex: 1, 
-        paddingTop:50,
-        
+        // paddingTop:50,
         // justifyContent: 'center',
         // alignItems: 'center',
         padding: 20,
@@ -297,35 +270,37 @@ const Styles = StyleSheet.create({
         backgroundColor: 'white',
         padding:10,
         // marginBottom:10,
+        textAlign:'right',
+
         borderRadius:5,
       },
     text:{
         fontWeight: 'bold',
-        color: 'white',
+        color: color.BLACK,
         fontSize: 20,
         paddingHorizontal:10,
         textAlign:'center'
         },
     textDetail:{
-    textAlign:'center',
-    fontSize:25,
-    fontWeight:'bold',
-    // paddingBottom:15,
-    paddingTop:15,
+        textAlign:'center',
+        fontSize:25,
+        fontWeight:'bold',
+        // paddingBottom:15,
+        paddingTop:15,
 
     },
     textColor:{
         fontWeight: 'bold',
-        color: 'white',
+        color: color.WHITE,
       },
     conTouch :{
         borderWidth:2,
-        borderColor:'#fff',
+        borderColor:color.WHITE,
         alignItems:'center',
         justifyContent:'center',
         width:150,
         height:50,
-        backgroundColor:'#009387',
+        backgroundColor:color.TURQUOISE,
         borderRadius:10,
         },
     center:{
@@ -335,14 +310,4 @@ const Styles = StyleSheet.create({
         // padding: 20,
         paddingBottom:100,
     },
-    //    centerRadio:{
-    //     alignItems:'center',
-    //     alignContent:'center',
-    //     // textAlign: 'center',
-    //     flexDirection: 'row',
-    //     justifyContent: 'center' ,
-    //     paddingBottom:20
-    //     // padding: 20,
-    //     // paddingBottom:100,
-    //    },
     })
