c33e3b5005d8ac4cff5f0b425b2c227db4ea8296
Adi Malachi <adida901@gmail.com>
2022-05-08 18:14:34 +0300

    export a data
    export a data

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,9 @@
 import * as color from './colors'
 export {color}
+
+
+import * as inputGiver from './inputGiver'
+export {inputGiver}
+
+import * as inputGetter from './inputGetter'
+export {inputGetter}
