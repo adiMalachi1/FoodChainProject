f98b7ab0f3a6b03c38277a17015e32df27c11902
Adi Malachi <adida901@gmail.com>
2022-06-07 12:36:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import RootStackScreen from "./navigation/RootStackScreen";
-import { 
-  NavigationContainer, 
-  DefaultTheme as NavigationDefaultTheme,
-  DarkTheme as NavigationDarkTheme
-} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 
-
-import { LogBox} from "react-native";
+import {LogBox} from "react-native";
 
 const App = () => {
   LogBox.ignoreLogs(['Setting a timer for a long period of time'])
   LogBox.ignoreLogs(['Each child in a list should have a unique "key" prop'])
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release.'])
 
   return(
-    <NavigationContainer>
-       
+    <NavigationContainer>      
       <RootStackScreen>
-
       </RootStackScreen>
-
     </NavigationContainer>
-  )
-  }
+  )}
 export default App;
 
