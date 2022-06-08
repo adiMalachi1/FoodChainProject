8eb597939a90e57b6e46b7552be4650e517b258c
Adi Malachi <adida901@gmail.com>
2022-06-08 17:35:42 +0300

    Update LoginScreen.js
    ordering and delete unnecessary things

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import { 
     View, 
     Text, 
@@ -8,25 +8,23 @@ import {
     Alert,
     Pressable,
     TextInput,
-    ColorPropType,
-    
+    StatusBar,
 } from 'react-native';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import {createUserWithEmailAndPassword } from "firebase/auth";
 import {auth,db} from '../FirebaseConfig';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { color } from '../utils';
+
 const LoginScreen = ({navigation}) => {
+  // const use states
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState();
   const [passError, setpassError] = useState();
-  // const [passwordVisible, setPasswordVisible] = useState(true);
   const [passwordVisibility, setPasswordVisibility] = useState(true);
-  const [rightIcon, setRightIcon] = useState('eye-off');
+  const [rightIcon, setRightIcon] = useState('eye-off'); // for hide password
 
 
-  const handlePasswordVisibility = () => {
+  const handlePasswordVisibility = () => { //handle function to show/not show password
     if (rightIcon === 'eye') {
       setRightIcon('eye-off');
       setPasswordVisibility(!passwordVisibility);
@@ -37,163 +35,141 @@ const LoginScreen = ({navigation}) => {
   };
 
 
-  const handleLogin =()=>{
-   
-      if(email==="" || password ==="" || (password.length < 6)){
+  const handleLogin =()=>{ // handle function for login to app + validation
+    
+    if(email==="" || password ==="" || (password.length < 6)){ //cant empty field, and password needs al least 6 characters
 
-        if(email=== ""){
-          Alert.alert('', "הודעת שגיאה: אמייל הינו שדה חובה",[,,{text:"אישור"}])
+          if(email=== ""){ // email is empty
+            Alert.alert('', "הודעת שגיאה: אימייל הינו שדה חובה",[,,{text:"אישור"}])
+            return;
+          }
+          else{
+            setEmailError("")
+            // return;
 
-          // return;
-        }
-        else{
-          setEmailError("")
-          // return;
+          }
+          if (password === ""){ //password is empty
+            Alert.alert('', "הודעת שגיאה: סיסמא הינה שדה חובה",[,,{text:"אישור"}])
 
-        }
-        if (password === ""){
-          Alert.alert('', "הודעת שגיאה: סיסמה הינה שדה חובה",[,,{text:"אישור"}])
-
-        }
-        else if (password.length < 6){
-          Alert.alert('', "הודעת שגיאה: על הסיסמא להיות בעלת 6 תווים לפחות",[,,{text:"אישור"}])
-        }
-        else{
-          setpassError("")
-
-        }
+          }
+          else if (password.length < 6){ //password less that 6 characters
+            Alert.alert('', "הודעת שגיאה: על הסיסמא להיות בעלת 6 תווים לפחות",[,,{text:"אישור"}])
+          }
+          else{
+            setpassError("")
+          }
       }
 
      else{
-      setpassError("")
-      auth.signInWithEmailAndPassword(email,password)
-      .then(userCredentials => {
-        var type
-        const user = userCredentials.user;
-        const userid = userCredentials.user.uid;
-        db.ref(`users/`+userid).on('value', function (snapshot) {
-          type = (snapshot.val().type)
-          if (type === 'giver'){
-            navigation.navigate("Tabs")
-          } 
-          else{
-            navigation.navigate("TabsGetter")
-  
-          }
-        })
-
-  
-        console.log('Logged in with:',user.email)
-       
-
+        setpassError("")
+        auth.signInWithEmailAndPassword(email,password) // sign in to firebase
+        .then(userCredentials => {
+          var type
+          const userid = userCredentials.user.uid;//get userid
+          db.ref(`users/`+userid).on('value', function (snapshot) { // ref to this user in firebase
+            type = (snapshot.val().type) //get the type of current user that sigin
+              navigation.navigate("Tabs", {type})
+        
+          })
       })
-        // .catch(error=>alert(error.message))
-        //  return;
-      .catch(error => {   
-        console.log(error)
-        alert(error.message)
-        // throw new Error(error.message);
-        switch(error.message) {
-          case "Can't find variable: db":
-            Alert.alert('', "הודעת שגיאה",[,,{text:"אישור"}])
-            break;
-          case 'Firebase: The email address is badly formatted. (auth/invalid-email).':
-                Alert.alert('', "הודעת שגיאה: כתובת אמייל לא תקינה",[,,{text:"אישור"}])
-                break;
-          case 'The email address is badly formatted.':
-            Alert.alert('', "הודעת שגיאה: כתובת אמייל לא תקינה",[,,{text:"אישור"}])
-            break;
-          case 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).':
-                Alert.alert('', "הודעת שגיאה: כתובת אמייל זו אינה קיימת, אנא צור חשבון קודם",[,,{text:"אישור"}])
-                break;
-          case 'There is no user record corresponding to this identifier. The user may have been deleted.':
-            Alert.alert('', "הודעת שגיאה: כתובת אמייל זו אינה קיימת, אנא צור חשבון קודם",[,,{text:"אישור"}])
-            break;
-          case 'Firebase: The password is invalid or the user does not have a password. (auth/wrong-password).':
-                Alert.alert('', "הודעת שגיאה: הסיסמא אינה תקינה, אנא נסה שנית",[,,{text:"אישור"}])
-                break;
-          case 'The password is invalid or the user does not have a password.':
-            Alert.alert('', "הודעת שגיאה: הסיסמא אינה תקינה, אנא נסה שנית",[,,{text:"אישור"}])
-            break;
-          case 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.':
-            Alert.alert('', "הודעת שגיאה: אירעה שגיאת רשת, אנא בדוק ונסה שנית",[,,{text:"אישור"}])
-            break;
-           
-       }
-     })
-      // .catch(error=>alert(error.message))
-      //    return;
+       
+        .catch(error => {    //catch error and show to user by alert
+          console.log(error)
+          // alert(error.message)
+          switch(error.message) {
+            case "Can't find variable: db":
+              Alert.alert('', "הודעת שגיאה",[,,{text:"אישור"}])
+              break;
+            case 'Firebase: The email address is badly formatted. (auth/invalid-email).':
+                  Alert.alert('', "הודעת שגיאה: כתובת אימייל לא תקינה",[,,{text:"אישור"}])
+                  break;
+            case 'The email address is badly formatted.':
+              Alert.alert('', "הודעת שגיאה: כתובת אימייל לא תקינה",[,,{text:"אישור"}])
+              break;
+            case 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).':
+                  Alert.alert('', "הודעת שגיאה: כתובת אימייל זו אינה קיימת, אנא צור חשבון קודם",[,,{text:"אישור"}])
+                  break;
+            case 'There is no user record corresponding to this identifier. The user may have been deleted.':
+              Alert.alert('', "הודעת שגיאה: כתובת אימייל זו אינה קיימת, אנא צור חשבון קודם",[,,{text:"אישור"}])
+              break;
+            case 'Firebase: The password is invalid or the user does not have a password. (auth/wrong-password).':
+                  Alert.alert('', "הודעת שגיאה: הסיסמא אינה תקינה, אנא נסה שנית",[,,{text:"אישור"}])
+                  break;
+            case 'The password is invalid or the user does not have a password.':
+              Alert.alert('', "הודעת שגיאה: הסיסמא אינה תקינה, אנא נסה שנית",[,,{text:"אישור"}])
+              break;
+            case 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.':
+              Alert.alert('', "הודעת שגיאה: אירעה שגיאת רשת, אנא בדוק ונסה שנית",[,,{text:"אישור"}])
+              break;
+            
+        }
+      })
+    
     }
   }
 
   return(
-    <View style={Styles.container}>
-    <View style =  {Styles.center}>
-    <Image
-      source={require('../assets/logoFoodChain.png')}
-      style={Styles.logo}
-    />
-     </View> 
-    
-    <Text style={Styles.textheader}>ברוכים הבאים!</Text>
-
-    <View style={Styles.inputContainer}>
-    <View style={Styles.inputContainer2}>
-
-    <TextInput
-        value={email}
-        onChangeText={(email) => setEmail(email)}
-        placeholder="אימייל"
-        activeUnderlineColor="white"
-        underlineColor='white'
-        selectionColor={"black"}
-        autoFocus={true}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-        style = {Styles.textInput}
-        
-      />
-      <MaterialCommunityIcons name={"email"} size={22} color="#232323" />
-
-      </View>
-        <Text style={Styles.errorMsg} >{emailError}</Text>
-        <View style={Styles.inputContainer2}>
-       <TextInput
-        value={password}
-        onChangeText={(password) => setPassword(password)}
-        placeholder="סיסמה" 
-        iconType="lock"
-        activeUnderlineColor="white"
-        underlineColor='white'
-        selectionColor={"black"}
-        secureTextEntry={passwordVisibility}
-
-        style = {Styles.textInput}
-       
-        />
-               
-        <Pressable onPress={handlePasswordVisibility}>
-    <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={color.TURQUOISE} />
+      <View style =  {styles.center}>
+        <Image
+          source={require('../assets/logoFoodChain.png')}
+          style={styles.logo}/>
+      </View> 
+      <Text style={styles.textheader}>ברוכים הבאים!</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.inputContainer2}>
+          <TextInput
+              value={email}
+              onChangeText={(email) => setEmail(email)}
+              placeholder="אימייל"
+              activeUnderlineColor={color.WHITE}
+              underlineColor={color.WHITE}
+              selectionColor={color.BLACK}
+              autoFocus={true}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style = {styles.textInput}    
+            />
+            <MaterialCommunityIcons name={"email"} size={22} color={color.BLACK} />
+        </View>
+          <Text style={styles.errorMsg} >{emailError}</Text>
+        <View style={styles.inputContainer2}>
+          <TextInput
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+            placeholder="סיסמא" 
+            iconType="lock"
+            activeUnderlineColor={color.WHITE}
+            underlineColor={color.WHITE}
+            selectionColor={color.BLACK}
+            secureTextEntry={passwordVisibility}
+            style = {styles.textInput}    
+            />          
+          <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons name={rightIcon} size={22} color={color.BLACK} />
           </Pressable>
         </View>
-         <Text style={Styles.errorMsg} >{passError}</Text>
+      </View> 
+      <View style = {styles.center}>
+          <TouchableOpacity onPress={handleLogin} style={styles.conTouch} >
+              <Text style = {[styles.textColor,{fontSize: 25,textAlign:'center'}]}>כניסה</Text>
+          </TouchableOpacity>
+          <TouchableOpacity  onPress={()=> navigation.navigate('מסך הרשמה')}>
+            <Text style = {[styles.textFor,{fontSize: 18,}]} >צור חשבון</Text>
+          </TouchableOpacity> 
+          <TouchableOpacity  onPress={()=> navigation.navigate('ForgotPassword')}>
+            <Text style = {styles.textFor} >שכחת סיסמא?</Text>
+          </TouchableOpacity> 
+      </View> 
 
-         </View> 
-         <View style = {Styles.center}>
-         <TouchableOpacity onPress={handleLogin}
-                style={Styles.conTouch} ><Text style = {[Styles.textColor,{fontSize: 25,}]}>כניסה</Text></TouchableOpacity>
-         <TouchableOpacity  onPress={()=> navigation.navigate('SignUpScreen')}><Text style = {[Styles.textFor,{fontSize: 18,}]} >צור חשבון</Text></TouchableOpacity> 
-         <TouchableOpacity  onPress={()=> navigation.navigate('ForgotPassword')}><Text style = {Styles.textFor} >שכחת סיסמא?</Text></TouchableOpacity> 
-
-         </View> 
     </View> 
     
     
   )}
   export default LoginScreen;
-  const Styles = StyleSheet.create({
-     
+  const styles = StyleSheet.create({
           container: {
             backgroundColor: color.TURQUOISE,
             flex: 1,
@@ -204,23 +180,21 @@ const LoginScreen = ({navigation}) => {
              width:'90%',
           },
           center:{
-            justifyContent: 'center',
             alignItems: 'center',
+            alignSelf:'center',
             padding:20,
           },
           textInput:{
-            backgroundColor: color.WHITE,
+            backgroundColor: color.WHITE_GRAY,
             padding:10,
             // marginBottom:10,
             textAlign:'right',
             borderRadius:5,
             width:'90%',
             
-           
           },
-       
           inputContainer2: {
-            backgroundColor: color.WHITE,
+            backgroundColor: color.WHITE_GRAY,
             borderRadius: 8,
             flexDirection: 'row',
             alignItems: 'center',
@@ -228,15 +202,18 @@ const LoginScreen = ({navigation}) => {
             borderColor: '#d7d7d7',
           },
           conTouch :{
+            marginTop:20,
             borderWidth:2,
-            color: color.WHITE,
-            alignItems:'center',
-            justifyContent:'center',
-            borderColor:color.WHITE,
+            color: color.WHITE_GRAY,
+            alignSelf:'center',
             width:150,
             height:50,
+            borderColor: color.WHITE_GRAY,
+            borderRadius: 10,
             backgroundColor:color.TURQUOISE,
-            borderRadius:10,
+            alignItems:'center',
+            justifyContent:'center',
+            alignContent:'center',
             },
           logo: {
             height: 85,
@@ -248,14 +225,14 @@ const LoginScreen = ({navigation}) => {
           },
           textheader:{
             fontWeight: 'bold',
-            color: color.WHITE,
+            color: color.WHITE_GRAY,
             fontSize: 35,
             paddingBottom:40,
             textAlign:'center'
           },
           textColor:{
             fontWeight: 'bold',
-            color: color.WHITE,
+            color: color.WHITE_GRAY,
             
           },
           // text: {
@@ -268,7 +245,7 @@ const LoginScreen = ({navigation}) => {
           textFor: {
             fontSize: 15,
             marginTop:10,
-            marginBottom: 10,
+            // marginBottom: 5,
             color: color.BLACK,
             fontWeight: 'bold',
           },
