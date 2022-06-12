3633f22cb625de109a3931ff77e92a03cfac2d9d
Adi Malachi <adida901@gmail.com>
2022-06-12 14:27:39 +0300

    Update tabs.js
    arranging screen + exit -logout alert

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -2,72 +2,62 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Dashboard from '../Screens/DashboardPage';
-import DashboardGetter from '../Screens/DashboardGetter';
 import HomePage from '../Screens/HomePage';
-import GettersPage from '../Screens/GettersPage';
+import SearchUser from '../Screens/SearchUser';
 import React from 'react';
-import { View, Text,StyleSheet, Button, Alert, } from 'react-native';
-import {Icon} from 'react-native-elements'
+import {Alert,Platform } from 'react-native';
 
-import { Directions, TouchableOpacity } from 'react-native-gesture-handler';
 import {auth} from '../FirebaseConfig';
 import { color } from '../utils';
-import SignInScreen from '../Screens/SignInScreen';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+//create tab navigator
 const Tab = createBottomTabNavigator();
 
 const Tabs = ({navigation, route} ) =>{
     const {type} = route.params;
-  let name
-  // screen
-  if(type === 'giver'){
-    name = 'נתרמים'
-    // screen = Dashboard
-  }
-  else{
-    name = 'תורמים'
-    // screen = DashboardGetter
-  }
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.navigate("HomeScreen")
-      })
-      .catch(error => alert(error.message))
-  }
-    return(
-            <Tab.Navigator 
-            
-            initialRouteName={"איזור אישי"}
-          
+    let name
+    // screen
+    if(type === 'giver'){
+      name = 'נתרמים'
+    }
+    else{
+      name = 'תורמים'
+    }
+
+    return(   
+        <Tab.Navigator     
+            initialRouteName={"איזור אישי"}   
             screenOptions={({ route }) => ({
-              
-           
-            
             headerTitle: route.name,
             headerShown:true,
-             
+                headerTitleAlign:'center',
                 headerStyle: {
                   backgroundColor: color.TURQUOISE,
                 },
-                headerTintColor: color.WHITE,  
+                headerTintColor: color.WHITE_GRAY,  
                 headerTitleStyle: {
                   fontWeight: 'bold',
-                 
+              
                 },
+              
                 headerRight: () => (
+                  Platform.OS === 'ios'?
+                      <MaterialCommunityIcons name={"arrow-right"} size={26} color={color.WHITE_GRAY} 
+                      style = {{right:10,}}
+                      onPress = {() => navigation.goBack()}
+                        />
+                      :
                   <Ionicons
                     name='log-out-outline'
                     size = {26}
                     style = {{right:10}}
-                    color ={color.WHITE}
+                    color ={color.WHITE_GRAY}
                     onPress = {()=>
                       Alert.alert('יציאה','האם אתה בטוח שאתה רוצה לצאת?',[
                       {
                         text:'כן',
-                        onPress: ()=>{ auth
-                          .signOut()
+                        onPress: ()=>{ 
+                          auth.signOut()
                           .then(() => {
                             navigation.navigate("HomeScreen")
                           })
@@ -75,15 +65,51 @@ const Tabs = ({navigation, route} ) =>{
                         
                       },{
                         text:'לא',
+                        onPress: ()=>{ 
+                          return
+                        },
                       }
                       ],
-                     
                       )
         
                     }/>),
+                
+                headerLeft:()=>(
+                  Platform.OS === 'ios'?
+                  <Ionicons
+                  name='log-out-outline'
+                  size = {26}
+                  style = {{left:10}}
+                  color ={color.WHITE_GRAY}
+                  onPress = {()=>
+                    Alert.alert('יציאה','האם אתה בטוח שאתה רוצה לצאת?',[
+                    {
+                      text: 'לא',
+                      onPress: ()=>{ 
+                          return
+                        
+                    }},{
+                      text: 'כן',
+                      onPress: ()=>{ 
+                            auth
+                            .signOut()
+                            .then(() => {
+                              navigation.navigate("HomeScreen")
+                          })
+                          .catch(error => alert(error.message))
+                        } 
+                      
+                    }
+                    ],)}/>
+                  :
+                  <MaterialCommunityIcons name={"arrow-right"} size={26} color={color.WHITE_GRAY} 
+                    style = {{left:10,}}
+                    onPress = {() => navigation.goBack()}
+                      />
+                ),
                 tabBarHideOnKeyboard: true,
                 tabBarActiveTintColor: color.TURQUOISE,
-                tabBarInactiveTintColor: "gray",
+                tabBarInactiveTintColor: color.GRAY,
                 tabBarLabelStyle: {
                   fontSize: 10
                 },
@@ -95,41 +121,27 @@ const Tabs = ({navigation, route} ) =>{
                     
                 tabBarIcon: ({ focused, color, size }) => {
                   let iconName;
-
-                  if (route.name === "בית") {
-                    iconName = focused ? 'home' : 'home-outline';
-      
-                  } else if (route.name === name) {
-                    iconName = focused ? 'people' : 'people-outline';
-      
-                  } else if (route.name === "איזור אישי") {
+                  if (route.name === "מפה") {
+                    iconName = focused ? 'map' : 'map-outline'; } 
+                  else if (route.name === name) {
+                    iconName = focused ? 'people' : 'people-outline';} 
+                  else if (route.name === "איזור אישי") {
                     iconName = focused ? 'person' : 'person-outline';
                   }
-      
-                  // You can return any component that you like here!
                   return <Ionicons name={iconName} size={size} color={color} />;
                 },
               })}>
         
-                 <Tab.Screen name="בית" component={HomePage}  />
-                 <Tab.Screen name={name} component={GettersPage} />
+                 <Tab.Screen name="מפה" component={HomePage}  />
+                 <Tab.Screen name={name} component={SearchUser} />
                  <Tab.Screen name="איזור אישי" component={Dashboard} />
  
 
-            </Tab.Navigator>
+        </Tab.Navigator>
 
     );
 }  
 
 
 export default Tabs;
-const Styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      // backgroundColor: '#009387',
-      backgroundColor: "#fff",
-      margin:10,
-      // alignItems: 'center',
-      paddingTop: 10, 
-    },
-  })
+
