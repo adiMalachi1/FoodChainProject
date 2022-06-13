4f5422b6a4a409d22c95bdbedd1431b271ea3605
Adi Malachi <adida901@gmail.com>
2022-06-13 17:46:56 +0300

    Update index.js
    add more colors and change names

--- utils/colors/index.js
+++ utils/colors/index.js
@@ -2,10 +2,11 @@
 export const ORANGE = '#EF6C00'
 export const TURQUOISE = '#4DB6AC'
 export const BLACK = '#000000'
-export const WHITE = '#F3F3F3'
+export const WHITE_GRAY = '#F3F3F3'
 export const BROWN = '#695D46'
 export const TURQUOISE1 = '#4DB6AC5E'
 export const TURQUOISE2 = '#4DB6AC2E'
-
-
+export const GRAY = 'gray'
+export const WHITE = 'white'
+export const ICONEYE = '#757575'
 
