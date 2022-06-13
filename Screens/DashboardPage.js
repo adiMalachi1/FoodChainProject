8ab1cf329708bbade59ad6029c2e0762f126939a
Adi Malachi <adida901@gmail.com>
2022-06-13 23:44:02 +0300

    Update DashboardPage.js
    Fixing not necessary things and arrange code

--- Screens/DashboardPage.js
+++ Screens/DashboardPage.js
@@ -5,233 +5,248 @@ import {
     TouchableOpacity, 
     StyleSheet,
     Image,
-    Button,
-    
+    Alert,
+    FlatList,
 } from 'react-native';
+import * as Device from 'expo-device';
 import { useState, useEffect } from 'react'
-import {auth, db, storage} from '../FirebaseConfig';
-import { Avatar,Title,
-    Caption, } from 'react-native-paper';
+import {auth, db} from '../FirebaseConfig';
+import { Title,Caption, } from 'react-native-paper';
 import { color } from '../utils';
-
+import * as Notifications from 'expo-notifications'
+import { ScrollView } from 'react-native-gesture-handler';
+import {StatusBar} from 'react-native';
 
 const DashboardPage = ({navigation}) => {
-    const [imageUrl, setImageUrl] = useState(undefined);
-    const [userObj, setUserObj] = useState('');
-    const [userObjForm, setUserObjForm] = useState('');
-    // const [tags, setTags] = useState('')
-    const [pick, setPick] = useState('')
-    const [time,setTime] = useState('')
-    const [food, setFood] = useState('')
 
-    const handleSignOut = () => {
-      auth
-        .signOut()
-        .then(() => {
-          navigation.replace("HomeScreen")
-        })
-        .catch(error => alert(error.message))
+  const [match, setMatch] = useState([]);//set all match
+  const [userObj, setUserObj] = useState('');//set user object data
+  const [userObjForm, setUserObjForm] = useState(''); //set user data from of user
+
+  useEffect(() => {
+    let unmounted = false
+    // console.log("ruuning effect to fetch data")
+    setTimeout(()=>{
+    // console.log("data loaded for page" )
+    if(!unmounted){
+      registerForPushNotificationsAsync();
+      }},1000)
+    return()=>{
+      unmounted = true
     }
+   },[]);
 
-    const checkData =(value)=>{
+  useEffect(() => {
+    let unmounted = false
+    // console.log("ruuning effect to fetch data")
+    setTimeout(()=>{
+      // console.log("data loaded for page" )
+      if(!unmounted){
+        if (auth.currentUser) {
+          const userid = auth.currentUser.uid; 
+          db.ref(`users/`+userid+'/match').on('value',  (snapshot) =>{
+            var users =[]
+            snapshot.forEach((child)=>{
+              if(child.val().status === true){
+              users.push({
+                key:child.key,
+                date:child.val().date,
+                name: child.val().with,
+              })
+              setMatch(users)
+            }
+            })
+          })
+          if (userid) {
+            db.ref('users/'+userid).on('value', (snapshot) => {
+              setUserObj(snapshot.val()) 
+              setUserObjForm(snapshot.val().Form) 
+
+          })
+
+          }
+          else{
+            Alert.alert('', "הודעת שגיאה: זהו לא המזהה הנכון",[,,{text:"אישור"}])
+          }}
+          else{
+            Alert.alert('', "הודעת שגיאה: זהו אינו משתמש פעיל במערכת, יתכן כי משתמש זה כבר מחובר ממכשיר אחר, אנא התנתק ונסה שנית",[,,{text:"אישור"}])
+        }}},1000)
+    return()=>{
+      unmounted = true
+    }
+  }, []);
+   
+   const registerForPushNotificationsAsync = async () => { //fuction to register to pusg notification
+     if (Device.isDevice) {
+       //check for existing permissions
+       const { status } = await Notifications.getPermissionsAsync();
+       let finalStatus = status;
+       //if no exisitng permissions, ask user from permission
+       if (status !== 'granted') {
+         const { status } = await Notifications.requestPermissionsAsync();
+         finalStatus = status;
+       }
+       //if no permissions, exit the function
+       if (finalStatus !== 'granted') {
+         alert('Failed to get push token for push notification!');
+         return;
+       }
+       //get pusk notification token
+       const token = (await Notifications.getExpoPushTokenAsync()).data;
+       console.log(token);
+       let uid = auth.currentUser.uid
+      //  alert(uid)
+       db.ref('users/' + uid ).update({
+        expoPushToken:token
+        
+      })   
+     } 
+     else {
+        Alert.alert("","הודעת שגיאה: חייב להשתמש במכשיר פיזי עבור הודעות Push",[,,{text:"אישור"}]);
+        return;
+     }
+   
+     if (Platform.OS === 'android') {
+       Notifications.setNotificationChannelAsync('default', {
+         name: 'default',
+         importance: Notifications.AndroidImportance.MAX,
+         vibrationPattern: [0, 250, 250, 250],
+         lightColor: '#FF231F7C',
+       });
+     }
+     };
+    
+    const checkCatogary=(catogary)=>{ //if catogary is other - returns what the user written instead 
+      if(catogary == "אחר")
+         return userObjForm.other
+      else{
+        return catogary
+      }
+    }
+    const checkData =(value)=>{ // check data exist and return as string with comma between each word
       try{
-        if(value.typeFood === undefined){
-            setFood('')
+        if(value === undefined){
+            return ""
         }
         else{
         let string = ""
-        for (var i=0; i < value.typeFood.length; i++) {
-           if(i !== value.typeFood.length-1 ){
-            string += (value.typeFood[i]+', ')
+        for (var i=0; i < value.length; i++) {
+           if(i !== value.length-1 ){
+            string += (value[i]+', ')
           }
-          else {
-            string += (value.typeFood[i])
-          }
-         
+          else {string += (value[i])}   
         }
-        setFood(string)
-        
+        return string
        }
       }
       catch(error){
         console.log(error)
-      }
-
-      // try{
-        
-      //   let string = ""
-      //   if(value.tags=== undefined){
-      //     setTags('')
-      //   }
-      //   else{
-      //   for (var i=0; i < value.tags.length; i++) {
-      //      if(i !== value.tags.length-1 ){
-      //       string += (value.tags[i]+', ')
-      //     }
-      //     else {
-      //       string += (value.tags[i])
-      //     }
-         
-      //   }
-
-      //   setTags(string)
-      //   // alert(tags)
-      //  }
-      // }
-      // catch(error){
-      //   console.log(error)
-      // }
-
-      try{
-        let string = ""
-        if(value.timePick === undefined){
-          setPick('')
-        }
-        else{
-          for (var i=0; i < value.timePick.length; i++) {
-            if(i !== value.timePick.length-1 ){
-              string += (value.timePick[i]+', ')
-            }
-            else {
-              string += (value.timePick[i])
-            }
-          
-          }
-          setPick(string)
-        }
-      }
-      catch(error){
-        console.log(error)
-      }
-      try{
-        let string = ""
-        if(value.Time === undefined){
-          setTime('')
-        }
-        else{
-          for (var i=0; i < value.Time.length; i++) {
-            if(i !== value.Time.length-1 ){
-              string += (value.Time[i]+', ')
-            }
-            else {
-              string += (value.Time[i])
-            }
-          
-          }
-          setTime(string)
-        }
-      }
-      catch(error){
-        console.log(error)
-      }
+      } 
     }
-    useEffect(() => {
-      if (auth.currentUser) {
-        const userid = auth.currentUser.uid;
-    
-       if (userid) {
-        storage
-          .ref('images/').child(userid) //name in storage in firebase console
-          .getDownloadURL()
-          .then((url) => {
-            setImageUrl(url);
-            // console.log(url)
-          })
-
-          .catch((e) => console.log('Errors while downloading => ', e));
-      }}}, []);
-
-      
-        useEffect(() => {
-          if (auth.currentUser) {
-            const userid = auth.currentUser.uid;
-        
-           if (userid) {
-             db.ref('users/'+userid).on('value', (snapshot) => {
-               setUserObj(snapshot.val()) 
-              
-           })
-              db.ref('users/'+userid+'/Form').on('value', (snapshot) => {
-              setUserObjForm(snapshot.val()) 
-              checkData(snapshot.val())
-            })
-          
-          }}}, []);
+      //put the information we need in fields by function
+      let catogary =  checkCatogary(userObjForm.catogary)
       let first,firstValue, second,secondValue
-      if (userObj.type === 'getter'){
+      let time,pickTime,typeFood
+      time = checkData(userObjForm.Time)
+      pickTime = checkData(userObjForm.timePick)
+      typeFood = checkData(userObjForm.typeFood)
+
+      //if getter/giver show somthing else
+      if (userObj.type === 'getter'){ 
           first = 'לכמה אנשים?'
           firstValue = userObjForm.numberPeople
           second = 'סוג המזון'
-          secondValue = food
-         
+          secondValue = typeFood
       }
-      else{
+      else{                        
           first = 'סוג המזון'
-          firstValue = food
+          firstValue = typeFood
           second = 'משקל (בק"ג)'
           secondValue = userObjForm.amount
-  
-  
       }
+      const renderSeparator = () => { //seperator between each item
+            return (
+              <View
+                style={styles.seperator}
+              />
+            )
+      }
+      const renderItem = (({ item } ) => (
+        <View style ={styles.item}>     
+          <Text style={styles.date}> 
+              {item.date}     
+          </Text>
+          <Text style={styles.name}>
+              {item.name}
+          </Text> 
+       </View>
+    ))
     return (
-        <View style ={Styles.container}>
-          
-            {/* <Image style={{height: 50, width: 50}} source={{uri: imageUrl}} /> */}
-            <View style={{paddingVertical:5,flexDirection:'column',justifyContent:'center', alignItems:'center'}}>
-            <Image style={{height: 80, width: 80, borderRadius:50}} source={{uri: imageUrl}} />
-                      <Title style={Styles.title}>{userObj.userName}</Title> 
-
-                      <Caption style={{fontSize:14,}}>{userObjForm.catogary}</Caption> 
-            </View>
-           
-          <View style ={{alignItems:'center',paddingTop:15,direction:'rtl'}} >
-            <View style={Styles.infoBoxWrapper}>
-            <Text style = {Styles.title}>{first}</Text>
-              <Caption style={Styles.caption}>{firstValue} </Caption>
-            </View>
-            <View style={Styles.infoBoxWrapper}>
-            <Text style = {Styles.title}>{second}</Text>
-              <Caption style={Styles.caption}>{secondValue}</Caption>
-            </View>
-            <View style={Styles.infoBoxWrapper}>
-              <Text style = {Styles.title}>מתי</Text>
-              <Caption style={Styles.caption}>{pick}</Caption>
-            </View>
-            <View style={Styles.infoBoxWrapper}>
-            <Text style = {Styles.title}>בין השעות</Text>
-              <Caption style={Styles.caption}>{time}</Caption>
-            </View>
-            </View>
-            <View style = {{ alignItems:'center'}}>
-
-            <TouchableOpacity
-                style={Styles.userBtn}
-                onPress={() => {
-                  navigation.navigate('EditProfile');
-                }}>
-                <Text style={Styles.userBtnTxt}>עריכת פרטי הטופס</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={Styles.userBtn} onPress={() => {
-                  navigation.navigate('עריכת פרטים אישיים');
-                }}>
-                <Text style={Styles.userBtnTxt}>עריכת פרטים אישיים</Text>
-              </TouchableOpacity >
-              <TouchableOpacity style={Styles.userBtn} onPress={handleSignOut}>
-                <Text style={Styles.userBtnTxt}>Logout</Text>
-              </TouchableOpacity >
+      <ScrollView>
+      <View style ={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor={color.TURQUOISE} />
+          <View style={styles.header}>
+          <Image style={styles.image} source={{uri: userObj.image}} />
+                <Title style={styles.title}>{userObj.userName}</Title> 
+                <Caption style={{fontSize:14,}}>{catogary}</Caption> 
+          </View>
+          <View style ={styles.box} >
+              <View style={styles.infoBox}>
+              <Text style = {styles.title}>{first}</Text>
+                <Caption style={styles.caption}>{firstValue} </Caption>
               </View>
-            
-        </View>
+              <View style={styles.infoBox}>
+              <Text style = {styles.title}>{second}</Text>
+                <Caption style={styles.caption}>{secondValue}</Caption>
+              </View>
+              <View style={styles.infoBox}>
+                <Text style = {styles.title}>ימים</Text>
+                <Caption style={styles.caption}>{pickTime}</Caption>
+              </View>
+              <View style={styles.infoBox}>
+              <Text style = {styles.title}>בין השעות</Text>
+                <Caption style={styles.caption}>{time}</Caption>
+              </View>
+            </View>
+            <View style = {styles.editButton}>
+            <TouchableOpacity
+                style={styles.userBtn}
+                onPress={() => {
+                  navigation.navigate('עריכת פרטי הטופס',{type:userObj.type});
+                }}>
+                <Text style={styles.userBtnTxt}>עריכת פרטי הטופס</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.userBtn,{marginVertical:10}]} onPress={() => {
+                  navigation.navigate('עריכת פרטים אישיים', {imageUser:userObj.image});
+                }}>
+                <Text style={styles.userBtnTxt}>עריכת פרטים אישיים</Text>
+              </TouchableOpacity >
+            </View>
+            <View >
+              <Text style={styles.textMatch}>החלפות שבוצעו</Text>
+              <FlatList
+                data={match}
+                extraData={match.sort((a, b) => a.date < b.date)}
+                style={{width:'90%',margin:10,}}
+                renderItem = {renderItem}
+                keyExtractor = {(item)=>item.key}
+                ItemSeparatorComponent = {renderSeparator}
+                scrollEnabled={false}/>
+            </View>
+      </View>
+      </ScrollView>
 
     );
 };
 
+//make this component available to the app
 export default DashboardPage;
-const Styles = StyleSheet.create({
+
+//define styling
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: color.WHITE,
-        // alignItems: 'center',
+        backgroundColor: color.WHITE_GRAY,
         paddingTop: 10, 
       },
       title: {
@@ -241,41 +256,91 @@ const Styles = StyleSheet.create({
       caption: {
         fontSize: 15,
         fontWeight: 'bold',
-        // padding:5,
-        // margin:5,
-        // lineHeight: 30,
       },
-      infoBoxWrapper: {
-        // borderBottomColor: '#dddddd',
-        // borderBottomWidth: 1,
+      box:{
+        alignItems:'center',
+        paddingTop:15,
+        direction:'rtl'
+      },
+      infoBox: {
         flexDirection: 'column',
         alignItems:'flex-start',
-        // height: '10%',
-        // justifyContent:'space-between',
         padding:5,
-        // height:'15%',
         width: '90%',
         borderTopColor: '#dddddd', 
         borderTopWidth: 2
       },
-      infoBox: {
-        width: '50%',
-        alignItems: 'center',
-        justifyContent: 'center',
+      header:{
+        paddingVertical:5,
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
       },
       userBtn: {
-        borderColor: 'black',
+        margin:5,
+        borderColor: color.WHITE_GRAY,
         borderWidth: 2,
-        borderRadius: 3,
+        borderRadius: 10,
         paddingVertical: 8,
         marginVertical: 5,
-        width:'50%', 
+        backgroundColor:color.TURQUOISE,
+        width:'40%', 
         alignItems:'center',
         justifyContent:'center',
         alignContent:'center',
-        
       },
       userBtnTxt: {
-        color: '#2e64e5',
+        color: color.WHITE_GRAY,
       },
+      textMatch:{
+        color:color.BLACK,
+        fontSize:16,
+        backgroundColor:color.TURQUOISE1,
+        width:'90%',
+        textAlign:'center',
+        alignSelf:'center',
+        padding:2,
+        marginTop:10
+      },
+      editButton:{
+        alignItems:'center',
+        alignSelf:'center',
+        ...Platform.select({
+          ios: {
+              flexDirection: 'row-reverse',
+          },
+          android:{
+              flexDirection: 'row',
+          },
+
+      }),
+      },
+      item:{
+        width:'100%', 
+        padding:10,
+        alignItems:'center',
+        justifyContent:'center'
+      },
+      name:{
+        color:color.BLACK,
+        fontSize:16,
+      },
+      image:{
+        height: 100, 
+        width: 100, 
+        borderRadius:50
+      },
+      date:{
+        color:color.BLACK,
+        fontSize:16,
+        fontWeight:'bold',
+        writingDirection:'rtl'
+      },
+      seperator:{
+        height: 2,
+        width: '90%',
+        backgroundColor: color.BLACK,
+        alignSelf:'center',
+        marginLeft: '5%',
+      }
     }) 
