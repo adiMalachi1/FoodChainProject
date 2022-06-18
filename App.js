d1bf6bcf4759630060297ec20aa71a8c1211c5f7
Adi Malachi <adida901@gmail.com>
2022-06-18 23:57:21 +0300

    Update App.js

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ const App = () => {
   LogBox.ignoreLogs(['Each child in a list should have a unique "key" prop'])
   LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release.'])
   LogBox.ignoreLogs(['PermissionsAndroid" module works only for Android platform.'])
+  LogBox.ignoreLogs(["Warning: Can't perform a React state update on an unmounted component"])
+  LogBox.ignoreLogs(["VirtualizedLists should never be nested inside plain"])
 
   return(
     <NavigationContainer>      
